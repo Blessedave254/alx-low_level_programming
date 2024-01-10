9f5c75f96824f654363ff9233cf3515162fd392f
Blessedave254 <102531614+Blessedave254@users.noreply.github.com>
2024-01-10 22:44:19 +0300

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int is_palindrome(char *s)
+{
+    if (s == NULL)
+        return 0;
+    int len = strlen(s);
+    if (len == 0 || len == 1)
+        return 1;
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (s[i] != s[len - i - 1])
+            return 0;
+    }
+    return 1;
+}
+int main(void)
+{
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
+}
