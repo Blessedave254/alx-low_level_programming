f190109e358d2296229f846299e612bba7f53502
Blessedave254 <102531614+Blessedave254@users.noreply.github.com>
2024-01-10 22:22:48 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return -1;
+    else if (n == 0)
+        return 0;
+    else
+        return sqrt_helper(n, 1);
+}
+
+int sqrt_helper(int n, int i)
+{
+    if (n == 1)
+        return 1;
+    else if (i * i == n)
+        return i;
+    else if (i * i > n)
+        return -1;
+    else
+        return sqrt_helper(n, i + 1);
+}
+int main(void)
+{
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
+}
