39307ebf80e8b9fa91a54f3fca58e715ba2a21dc
Blessedave254 <102531614+Blessedave254@users.noreply.github.com>
2024-01-12 04:36:27 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,6 +6,11 @@ int _strlen_recursion(char *s)
         return 0;
         return 1 + _strlen_recursion(s + 1);
 }
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
  int n;
